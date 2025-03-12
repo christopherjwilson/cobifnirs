d457a235ff4b3478432e1621244eb3e174911cf4
chris <christopher.wilson@tees.ac.uk>
2025-03-12 10:41:43 +0000

    change get_event_data params from "blockName" to "eventName". Want to create a different function for block summaries.

--- R/get_event_data.R
+++ R/get_event_data.R
@@ -3,18 +3,16 @@
 #'
 #'The function will look for matching start and end markers (so will assume that each event has a start and end marker). It will then extract the data between the start and end markers and return a list of data frames, one for each event. The data frames will have a trial column that specifies the event number. The function will also add a block column to the data that specifies the block name (inputted as a parameter).
 #'@param data A data frame with the fnirs data. It should have been imported using the \code{\link{import_nirs}} function and had markers added using the \code{\link{add_markers}} function.
-#'@param blockStart The name of the start marker for blocks of trials in the data. This should be a string and it should match a value in the marker column.
-#'@param blockEnd The name of the end marker for blocks of trials in the data. This should be a string and it should match a value in the marker column.
 #'@param eventStart The name of the start marker for discrete events/trials in the data. This should be a string and it should match a value in the marker column.
 #'@param eventEnd The name of the end marker for discrete events/trials in the data. This should be a string and it should match a value in the marker column.
 #'@param lead The number of data points to lead the start marker by. This is useful because of the lead/lag of fnirs responses to events. The default is 0.  To calculate seconds, divide by the sample rate (e.g. at 2 Hz would be a value of 10 for 5 seconds).
 #'@param lag The number of data points to lag the end marker by. This is useful because of the lead/lag of fnirs responses to events. The default is 0. To calculate seconds, divide by the sample rate (e.g. at 2 Hz would be a value of 10 for 5 seconds).
-#'@param blockname The name of the block. This is for the convenience of the researcher, to be able to distinguish between different blocks of trials, or different tasks. This should be a string and will be added as a column to the data.
+#'@param eventName The name of the block. This is for the convenience of the researcher, to be able to distinguish between different blocks of trials, or different tasks. This should be a string and will be added as a column to the data.
 #'@return A list of data frames, one for each event. Each data frame will have a trial column that specifies the event number and a block column that specifies the block name.
 #'@import dplyr
 #'@export
 
-get_event_data <- function(data, eventStart, eventEnd, blockname, lead = 0, lag = 0) {
+get_event_data <- function(data, eventStart, eventEnd, eventName, lead = 0, lag = 0) {
 
   data$t <- as.numeric(data$t)
   data$marker <- as.character(data$marker)
@@ -137,7 +135,7 @@ if(lead > 0 | lag > 0){
     getData <- function(i, timeData, data){
 
 
-      trial_data <- data %>% filter(t >= timeData$start[[i]] & t <= timeData$end[[i]]) %>% mutate(trial = i, block = blockname)
+      trial_data <- data %>% filter(t >= timeData$start[[i]] & t <= timeData$end[[i]]) %>% mutate(trial = i, event = eventName)
 
 
 
