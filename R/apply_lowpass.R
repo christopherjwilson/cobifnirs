0231e4e3123150b2a21ef333d27b3a83b02b3780
chris <christopher.wilson@tees.ac.uk>
2025-03-17 13:53:41 +0000

    updated namespace with new functions

--- R/apply_lowpass.R
+++ R/apply_lowpass.R
@@ -1,13 +1,13 @@
-#' @title Apply lowpass filter to fnirs data.
-#' @description This function will apply a lowpass filter to the fnirs data. The frequency of the filter and the window can be specified in the function. The output is a dataframe with the lowpass filter applied to the data, and the original data is preserved with the suffix "_pre_lp" added to the column names. The function uses a fir1 filter from the gsignal package, with a filter length of 2. The filter is scaled by dividing by the sum of the coefficients, and the starting point values are created using the filter_zi function. The filter is applied to the data, and the delay is calculated using the grpdelay function. The delay is then compensated for by shifting the data. The output is a dataframe with the lowpass filter applied to the data.
-#' @param oxyData (dataframe)  NIRS data that has been imported using the \code{\link{import_nirs}} function.
-#' @param freq (numeric) The frequency of the lowpass filter. The default is 0.1 (10Hz).
-#' @param w (numeric) The window of the lowpass filter. The default is 20.
-#' @return A dataframe with the lowpass filter applied to the data. Original data is preserved with the suffix "_pre_lp" added to the column names. The nirValue column is replaced with the lowpass filtered values.
-#' @import dplyr
-#' @import gsignal
+#'@title Apply lowpass filter to fnirs data.
+#'@description This function will apply a lowpass filter to the fnirs data. The frequency of the filter and the window can be specified in the function. The output is a dataframe with the lowpass filter applied to the data, and the original data is preserved with the suffix "_pre_lp" added to the column names. The function uses a fir1 filter from the gsignal package, with a filter length of 2. The filter is scaled by dividing by the sum of the coefficients, and the starting point values are created using the filter_zi function. The filter is applied to the data, and the delay is calculated using the grpdelay function. The delay is then compensated for by shifting the data. The output is a dataframe with the lowpass filter applied to the data.
+#'@param oxyData (dataframe)  NIRS data that has been imported using the \code{\link{import_nirs}} function.
+#'@param freq (numeric) The frequency of the lowpass filter. The default is 0.1 (10Hz).
+#'@param w (numeric) The window of the lowpass filter. The default is 20.
+#'@return A dataframe with the lowpass filter applied to the data. Original data is preserved with the suffix "_pre_lp" added to the column names. The nirValue column is replaced with the lowpass filtered values.
+#'@import dplyr
+#'@import gsignal
 #' @export
-#' @examples
+#'@examples
 #' \dontrun{
 #' # Assuming that the fnirs data has been imported using the \code{\link{import_nirs}} function.
 #' apply_lowpass(nirData, freq = 0.1, w = 20)
